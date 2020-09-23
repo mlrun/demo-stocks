dc0589bf8e05e8413ad1c4d070fcb32b68e3d0b8
Or Zilberman <zilbermanor@gmail.com>
2020-09-23 07:01:16 +0000

    Updated project and workflow

--- code/workflow.py
+++ code/workflow.py
@@ -96,11 +96,14 @@ def kfpipeline(
                                                             'EXPRESSION_TEMPLATE': EXPRESSION_TEMPLATE})
     
     stream_viewer = funcs['stream_viewer'].deploy_step(env={'V3IO_CONTAINER': V3IO_CONTAINER,
-                                                            'STOCKS_STREAM': STOCKS_STREAM})
+                                                            'STOCKS_STREAM': STOCKS_STREAM}).after(news_reader)
+    
+    grafana_builder = funcs['grafana'].deploy_step(skip_deployed=True)
     
     grafana_dashboard = funcs['grafana'].as_step(name='grafana_deployer',
-                                                 params={streamview_url: stream_viewer.outputs['endpoint'],
-                                                         v3io_container: V3IO_CONTAINER,
-                                                         stocks_kv_table: STOCKS_KV_TABLE,
-                                                         stocks_tsdb_table: STOCKS_TSDB_TABLE})
+                                                 params={'streamview_url': stream_viewer.outputs['endpoint'],
+                                                         'v3io_container': V3IO_CONTAINER,
+                                                         'stocks_kv_table': STOCKS_KV_TABLE,
+                                                         'stocks_tsdb_table': STOCKS_TSDB_TABLE},
+                                                 image=grafana_builder.outputs['image'])
     
